d9b6d2b88ec93f1500c0bb25a2d5fdee04169d5e
Christian F. Coors <ccoors@uni-bremen.de>
2017-08-12 21:23:02 +0200

    Fix typo

--- tools/musicrenderer/src/logurucache.cpp
+++ tools/musicrenderer/src/logurucache.cpp
@@ -1,7 +1,7 @@
 /**
  * This file does the loguru implementation.
  * 
- * It serves the purpose of being static, so logugu doesn't have to be compiled every time something changes.
+ * It serves the purpose of being static, so loguru doesn't have to be compiled every time something changes.
  */
 #define LOGURU_IMPLEMENTATION 1
 #include <loguru.hpp>
