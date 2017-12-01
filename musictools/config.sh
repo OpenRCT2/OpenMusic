c5f625865ca368eaac74eb20b09af1c2017677ce
Christian F. Coors <ccoors@uni-bremen.de>
2017-12-01 22:02:50 +0100

    Change sampling rate to 44100 Hz

--- musictools/config.sh
+++ musictools/config.sh
@@ -3,14 +3,14 @@ export RELEASE=release
 export SONGNAMES=songnames
 export SOUNDFONTSDIR=soundfonts
 
-export SAMPLING_RATE=48000
+export SAMPLING_RATE=44100
 export NORMALIZE_GAIN=-1
 export REVERB_DECAY=0.5
 
 export RELEASEDIR=$OUTDIR/$RELEASE
 export SONGNAMEDIR=$OUTDIR/$SONGNAMES
 
-export SOUNDFONT_ORGAN=BureaFuneralChapel.sf2
+export SOUNDFONT_ORGAN=Organ.sf2
 export SOUNDFONT_GENERALUSER="GeneralUser GS v1.471.sf2"
 export SOUNDFONT_FLUIDR3=FluidR3_GM.sf2
 export SOUNDFONT_NICEKEYS_UPRIGHT=Nice-Upright-Piano-sf2-JNv1.3.sf2
