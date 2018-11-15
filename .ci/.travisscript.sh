00d5bac875ac9fb111781319603256b721854b4a
Christian Friedrich Coors <me@ccoors.de>
2018-11-15 23:29:25 +0100

    Fix CI

--- .ci/.travisscript.sh
+++ .ci/.travisscript.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-echo "Building OpenRCT2-OpenMusic inside Travis CI"
+echo "Building OpenRCT2/OpenMusic inside Travis CI"
 
 pacman -Syu --noconfirm
 pacman -S --noconfirm base base-devel shadow sudo git python python-requests python-regex python-dateutil python-feedparser pyalpm expac ninja ttf-dejavu graphviz libsmf lv2 lilv cmake lilypond fluidsynth calf sox opus-tools curl openssh
@@ -27,17 +27,3 @@ cd /root/build
 ./getdependencies.sh --agree
 ./geninja.sh
 ./build.sh
-
-if [ "$1" == "master" ]
-then
-  echo "Uploading artifacts..."
-  mkdir -p /root/.ssh
-  echo "phoenix.uberspace.de,2001:1a50:11:0:5f:8f:ac:87,95.143.172.135 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHV8sbMwG3+68uuUFxRVPqrtYWVPu6N+9vujkriP6U15" >> /root/.ssh/known_hosts
-  cd out/release
-  scp -i ../../sshkey -r . "cuervo@phoenix.uberspace.de:html/openrct2-openmusic/ci/"
-  cd ../..
-else
-  echo "Not uploading artifacts due to being on branch $1"
-fi
-
-shred -u sshkey
